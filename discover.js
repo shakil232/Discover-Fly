6845a8d0970e8157a02c721168d561bbc57a604a
unknown <dxshakil2@gmail.com>
2021-02-01 08:32:44 -0800

    i quit

--- discover.js
+++ discover.js
@@ -1,51 +1,27 @@
 
 // First-Class-Ticket-handler....
 function TicketHandel(ticket,isIncrease){
-    const FirstClassTicketInput = document.getElementById(ticket +'-count');
-    const FirstClassTicketCount = parseInt(FirstClassTicketInput.value);
-    let FirstClassTicketNewCount =  FirstClassTicketCount;
+    const TicketInput = document.getElementById(ticket +'-count');
+    const TicketCount = parseInt(TicketInput.value);
+    let TicketNewCount = TicketCount;
     if( isIncrease==true){
-        FirstClassTicketNewCount =  FirstClassTicketCount +1;
+        TicketNewCount =  TicketCount +1;
     }
-    if( isIncrease==false && FirstClassTicketCount >0){
-        FirstClassTicketNewCount = FirstClassTicketCount -1;
+    if( isIncrease==false && TicketCount >0){
+        TicketNewCount = TicketCount -1;
     }
-    FirstClassTicketInput.value = FirstClassTicketNewCount;
+    
+    let ticketPrice = document.getElementById(ticket +'First-Classticket') ;
+    totalFirstPrice = ticketPrice *150;
+    let ticketPricee = document.getElementById(ticket +'Economy-Classticket') ;
+    totalEconomyPrice = ticketPricee *100;
+    let totalAmount = totalFirstPrice +  totalEconomyPrice;
+
+    TicketInput.value = TicketNewCount;
+    calculate(totalAmount);
 }
+function calculate (){
+    document.getElementById('total-price').innerText = '&' +totalAmount;
 
 
-
-
-
-// // First-Class-Ticket-handler....
-// function FirstClassTicketHandler (isIncrease){
-//     const FirstClassTicketInput = document.getElementById('First-Classticket-count');
-//     const FirstClassTicketCount = parseInt(FirstClassTicketInput.value);
-//     let FirstClassTicketNewCount =  FirstClassTicketCount;
-//     if( isIncrease==true){
-//         FirstClassTicketNewCount =  FirstClassTicketCount +1;
-//     }
-//     if( isIncrease==false && FirstClassTicketCount >0){
-//         FirstClassTicketNewCount = FirstClassTicketCount -1;
-//     }
-//     FirstClassTicketInput.value = FirstClassTicketNewCount;
-//     let totalFirstClassTicketPrice =  FirstClassTicketNewCount *150;
-   
-// }
-
-// // Economy-Class-Ticket-handler....
-// function EconomyClassTicketHnadler (isIncrease){
-//     const EconomyClassTicketInput = document.getElementById('Economy-Classticket-count');
-//     const EconomyClassTicketCount = parseInt(EconomyClassTicketInput.value);
-    
-//     let EconomyClassTicketNewCount = EconomyClassTicketCount ;
-//     if ( isIncrease == true){
-//         EconomyClassTicketNewCount = EconomyClassTicketCount +1;
-//     }
-//     if ( isIncrease == false && EconomyClassTicketCount >0){
-//         EconomyClassTicketNewCount = EconomyClassTicketCount -1;
-//     }
-//     EconomyClassTicketInput.value = EconomyClassTicketNewCount;
-//     let totalEconomyClassTicketPrice = EconomyClassTicketNewCount *100;
-    
-// }
+}
